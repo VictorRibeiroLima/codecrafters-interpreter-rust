3a547a2dcd05982c4c5ea596e4b9cb688f88eb69
Victor Ribeiro de Lima <victorlima.12@hotmail.com>
2024-07-08 06:00:23 -0300

    Scanning: Parentheses

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use std::env;
 use std::fs;
 use std::io::{self, Write};
 
+mod tokenizer;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     if args.len() < 3 {
@@ -14,18 +16,14 @@ fn main() {
 
     match command.as_str() {
         "tokenize" => {
-            // You can use print statements as follows for debugging, they'll be visible when running tests.
-            writeln!(io::stderr(), "Logs from your program will appear here!").unwrap();
-
             let file_contents = fs::read_to_string(filename).unwrap_or_else(|_| {
                 writeln!(io::stderr(), "Failed to read file {}", filename).unwrap();
                 String::new()
             });
 
-            if !file_contents.is_empty() {
-                panic!("Scanner not implemented");
-            } else {
-                println!("EOF  null"); // Placeholder, remove this line when implementing the scanner
+            let tokens = tokenizer::tokenize(&file_contents);
+            for token in tokens {
+                println!("{}", token);
             }
         }
         _ => {
---
+++ src/tokenizer/mod.rs
@@ -0,0 +1,126 @@
+use std::{fmt::Display, iter::Peekable, str::Chars};
+
+struct Keyword_ {
+    name: &'static str,
+    token: Token,
+}
+
+const RESERVED_KEYWORDS: [Keyword_; 1] = [Keyword_ {
+    name: "var",
+    token: Token::Var,
+}];
+
+#[derive(Clone)]
+pub enum Token {
+    Var,
+    Identifier(String),
+    Equal,
+    String(String),
+    Number(String),
+    Semicolon,
+    LeftParen,
+    RightParen,
+    Invalid(String),
+    EOF,
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Token::Var => write!(f, "VAR var null"),
+            Token::Identifier(s) => write!(f, "IDENTIFIER {} null", s),
+            Token::Equal => write!(f, "EQUAL = null"),
+            Token::String(s) => write!(f, "STRING {} \"{}\"", s, s),
+            Token::Number(s) => write!(f, "NUMBER {} {}", s, s),
+            Token::Semicolon => write!(f, "SEMICOLON ; null"),
+            Token::LeftParen => write!(f, "LEFT_PAREN ( null"),
+            Token::RightParen => write!(f, "RIGHT_PAREN ) null"),
+            Token::EOF => write!(f, "EOF  null"),
+            Token::Invalid(s) => write!(f, "INVALID {} null", s),
+        }
+    }
+}
+
+pub fn tokenize(input: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let mut chars = input.chars().peekable();
+
+    while let Some(c) = chars.next() {
+        match c {
+            ' ' => continue,
+            '=' => tokens.push(Token::Equal),
+            ';' => tokens.push(Token::Semicolon),
+            '(' => tokens.push(Token::LeftParen),
+            ')' => tokens.push(Token::RightParen),
+            '0'..='9' => {
+                let number = tokenize_number(c, &mut chars);
+                tokens.push(number);
+            }
+            '"' => {
+                let string = tokenize_string(&mut chars);
+                tokens.push(string);
+            }
+            'a'..='z' | 'A'..='Z' => {
+                let identifier = tokenize_identifier(c, &mut chars);
+                tokens.push(identifier);
+            }
+            _ => panic!("Unexpected character: {}", c),
+        }
+    }
+
+    tokens.push(Token::EOF);
+    tokens
+}
+
+fn tokenize_number(first_char: char, chars: &mut Peekable<Chars>) -> Token {
+    let mut number = String::new();
+    number.push(first_char);
+    while let Some(&c) = chars.peek() {
+        if c.is_digit(10) {
+            number.push(c);
+            chars.next();
+        } else if c == ' ' || c == ';' {
+            break;
+        } else {
+            number.push(c);
+            chars.next();
+            return Token::Invalid(number);
+        }
+    }
+    Token::Number(number)
+}
+
+fn tokenize_string(chars: &mut Peekable<Chars>) -> Token {
+    let mut string = String::new();
+    let mut last_char = '"';
+    while let Some(c) = chars.next() {
+        last_char = c;
+        if c == '"' {
+            break;
+        }
+        string.push(c);
+    }
+    if last_char != '"' {
+        return Token::Invalid(string);
+    }
+    Token::String(string)
+}
+
+fn tokenize_identifier(first_char: char, chars: &mut Peekable<Chars>) -> Token {
+    let mut identifier = String::new();
+    identifier.push(first_char);
+    while let Some(&c) = chars.peek() {
+        if c.is_alphanumeric() {
+            identifier.push(c);
+            chars.next();
+        } else {
+            break;
+        }
+    }
+    for keyword in RESERVED_KEYWORDS.iter() {
+        if keyword.name == identifier {
+            return keyword.token.clone();
+        }
+    }
+    Token::Identifier(identifier)
+}
